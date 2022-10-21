c82618d57f15dd6639f5f2e6686ceaee173c88eb
Hari Prasad <harimohan0502@gmail.com>
2022-10-21 21:05:15 +0530

    Add existing project files to Git

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -3,12 +3,12 @@ import logo from '../assets/logo.png';
 
 const Nav = () => {
     return(
-        <div className="w-full h-1/5 flex items-center justify-between p-4">
-            <img src={logo} className="w-16" alt="logo"/>
-            <div className="w-2/5 flex items-center justify-evenly">
-                <p className="text-xs font-thin">About</p>
-                <p className="text-xs font-thin">How to?</p>
-                <p className="text-xs font-thin">Docs</p>
+        <div className="w-full h-1/6 flex items-center justify-between p-4">
+            <a href="" ><img src={logo} className="w-16" alt="logo"/></a>
+            <div className="w-2/6 flex items-center justify-evenly">
+                <a href="#" ><p className="text-xs font-thin">About</p></a>
+                <a href="#" ><p className="text-xs font-thin">How to?</p></a>
+                <a href="#" ><p className="text-xs font-thin">Docs</p></a>
             </div>
         </div>
     )
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -45,7 +45,7 @@ const Home = () => {
             </div>
             <div className="w-full h-1/5 flex items-center flex-col justify-evenly">
                 <p className="text-sm text-rose-500	">NOTE</p>
-                <p className="text-xs">connect all devices in the same wifi</p>
+                <p className="text-xs font-weight-light">	• connect all devices in the same wifi</p>
             </div>
             {
                 enter && (
@@ -55,10 +55,10 @@ const Home = () => {
                          animate="visible"
                          className="absolute z-10 top-1/4 w-full h-3/4 bg-slate-900 flex items-center justify-center">
                         <div className="w-2/4 h-4/5 flex items-center justify-evenly flex-col">
-                            <p className="text-xs text-gray-300">Before entering</p>
-                            <p className="text-gray-50">Give yourself a cool Username</p>
-                            <input className="rounded  border-0 outline-0 py-1.5 w-2/4 px-3 text-xs" type="text" placeholder="John Doe"/>
-                            <button className="rounded btn p-1 bg-white text-xs text-white w-2/4 border-0 outline-0">ENTER</button>
+                            <p className="text-xs text-gray-500">Before entering</p>
+                            <p className="text-base text-gray-50">Give yourself a cool Username</p>
+                            <input className="rounded  border-0 outline-0 py-1.5 w-2/4 px-3 text-xs text" type="text" placeholder="John Doe"/>
+                            <button className="rounded btn p-1 bg-white text-xs py-1.5 text-white w-2/4 border-0 outline-0">ENTER</button>
                             <p className="cursor-pointer text-gray-300 text-xs underline" onClick={()=> handleEnter()}>Go back</p>
                         </div>
                     </motion.div>
